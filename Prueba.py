3ba97a46995574ea31070989362da53bbb1d10d6
jpma01 <122122643+jpma01@users.noreply.github.com>
2024-02-26 18:56:11 -0500

    Revisión uso de Función  1 a 5.
    Se corrige el uso de la función de entrada, para utilizarla en la generación de dos valores aleatorios que incrementan el rango de la salida.
    
    Se elimina el uso del tiempo del sistema y se agrega la comprobación por histograma de distribución aleatoria de 15000 ciclos.

--- Prueba.py
+++ Prueba.py
@@ -1,12 +1,14 @@
 ## Juan Pablo Mariño 
-# 22/02/2024
+# 26/02/2024
 # Al problema :
 """
 Crear una función (Fn2) que genere numeros aleatorios enteros de 1 a 7 
 que  reciba de entrada o use otra función (Fn1) que genera números aleatorios enteros entre 1 a 5.
+ 
+Actualización: se elimina el uso del tiempo del sistema como generador de semillas pseudo aleatorias,
+y se agrega la comprobación de  15.000 iteraciones para gráficar el histograma de distribución aleatoria.
 """
 import random
-import time
 import matplotlib.pyplot as plt
 
 
@@ -18,30 +20,29 @@ def FnEntrada():
     resultado = random.randint(1,5)
     return resultado
 
-def FnSalida(Entrada): 
+def FnSalida7():
     """
-    Entrega un número aleatorio entero entre 1 y 7 usando como semilla la entrada de la función 1.
+    Entrega un número aleatorio entero entre 1 y 7 apartir de los numeros aleatorios de la FnEntrada que arroja valores enteros de 1 a 5.
     """
-    if (Entrada >=1 and Entrada<=5 ) and isinstance(Entrada,int): #Evalua la entrada entre 1 a 5 y que sea entero.
-        Factor=time.time()# Extrae el valor desde el Epoch en el CPU en segundos 
-        if Entrada>3:# divide el comportamiento de la salida con base a la entrada
-            resultado=((12345+Entrada*Factor*1103515245)%0x7fffffff)%7 # Pseudoaleatorios con base en algoritmos GLC limitado a respuestas de 0 a 6.
-            # Toma una semilla como multiplicador del tiempo Epoch que tiene el procesador para generar datos pseudo aleatorios.                
-            resultado=round(resultado)+1    
-        else: 
-            resultado=((Factor+Entrada*1103515245)%0x7fffffff)%7# Pseudoaleatorios con base en algoritmos GLC limitado a respuestas de 0 a 6.
-            # Toma una semilla como Sumador del tiempo Epoch que tiene el procesador para generar datos pseudo aleatorios.                
-            resultado=round(resultado)+1  
-        return resultado
-    else :                
-        print("No válido: ", Entrada)
-        return Entrada
+    while True: # mientras que el comprobador sea menor a 
+        Resultado = 5 * (FnEntrada() - 1) + (FnEntrada()- 1) # Genera un rango de valores más grande, en este caso de (0 a 24)
+        if Resultado < 21: # se verifica cuando el Rango del Comprobador sea menor a 14 o 21 tiene que ser múltiplo de 7 mayor a 7
+            #(depende de la cantidad de números invocados de FnEntrada)       
+            Resultado=Resultado % 7 + 1 # Normalización de valores de 1 a 7
+            return  round(Resultado)
         
 
 ## Comprobación
-"""
-R1=FnEntrada()
-#R1=R1 ## Para modificar la entrada.
-R2=FnSalida(R1)
-print("Entrada :",R1," Salida: ", R2)
-"""
+num=FnSalida7()
+print("Numero Aleatorio= ", num)
+
+Lista_R2 = []
+for i in range (15000): # para evaluar multiples veces >10.000
+    R2=FnSalida7() 
+    Lista_R2.append(R2)
+#Histograma 
+His2=plt.hist(Lista_R2,bins=7) #plot del histograma de salida
+plt.xlabel('Valores')
+plt.ylabel('Frecuencia')
+plt.title('Lista Función FnSalida7')
+plt.show()
